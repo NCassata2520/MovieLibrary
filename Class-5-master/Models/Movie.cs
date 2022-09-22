1a5841466907d2292c18748a5c90ba8c7e6a38c0
NCassata2520 <ncassata@my.wctc.edu>
2022-09-21 21:41:40 -0500

    Add project files.

---
+++ Class-5-master/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_5.Models
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public List<string> Genres{ get; set; }
+    }
+}
---
+++ Class-5-master/Services/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Class_5.Services;
+
+/// <summary>
+///     This service interface only exists an example.
+///     It can either be copied and modified, or deleted.
+/// </summary>
+public interface IFileService
+{
+    void Read();
+    void Write(int movieId, string movieTitle, string genresString);
+    void Display();
+     List<int> MovieIds { get; set; }
+     List<string> MovieTitles { get; set; }
+     List<string> MovieGenres { get; set; }
+}
